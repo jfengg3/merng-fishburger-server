39ac94a6396c2258ba979fce2f25125d628bee8e
Hjenry <evelyst21@gmail.com>
2021-10-05 15:32:57 +0800

    init

---
+++ models/User.js
@@ -0,0 +1,12 @@
+// USER DATA SCHEMA
+
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+    username: String,
+    password: String,
+    email: String,
+    createdAt: String
+});
+
+module.exports = model('User', userSchema);
